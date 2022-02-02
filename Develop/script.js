2507e46bad08caa81c3e3dd57d2dbf744fa5bbe0
Lawrence Blundo <lawrs2022@gmail.com>
2022-02-02 00:12:24 -0500

    Added updated weather infor based on text input from user

--- Develop/script.js
+++ Develop/script.js
@@ -1,37 +1,58 @@
 // JS file used to retrieve forecasts from OpenWeather One Call api site
 
+// api key
+
+ApiKey = "1fc359ee9b12b02ee9633470d8821b6b";
+
 // Variable assignment for search button to acivate the fecth function
-var fecthBtn = document.getElementById("fetch-btn");
+var fetchBtn = document.getElementById("fetch-btn");
 
 // Variable for the saved locations list
-var saveLoc = document.querySelector(".saved-locals");
+var saveLoc = document.querySelector("saved-locals");
+
+// variable for city name
+var Cities = document.querySelector("city");
+var chosen = $("#selected-city");
 
 // variable for current weather data
-var currentWx = document.querySelector(".current-wx");
-var TempC = document.querySelector(".temp-c");
-var WindC = document.querySelector(".wind-c");
-var HumidC = document.querySelector(".humid-c");
-var UVC = document.querySelector(".UV-c");
-var CondC = document.querySelector(".cond-c");
+var nowcast = document.querySelector(".nowcast");
+var wxImg = document.createElement("img")
+
+var City = document.getElementById("city");
+var TempC = document.getElementById("temp-c");
+var WindC = document.getElementById("wind-c");
+var HumidC = document.getElementById("humid-c");
+var UVC = document.getElementById("UV-c");
+var CondC = document.getElementById("cond-c");
 
 // variable for future weather data
 var futureWx = document.querySelector(".future-wx");
 
+// Universal weather condition image icon
+
 // fetch function for OpenWeather One Call
 
 function getApi() {
-    var requetURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely,alerts&units=imperial&appid=1fc359ee9b12b02ee9633470d8821b6b";
-    fetch(requetURL)
-        .then(function(response) {
-            console.log(response);
-            return response.json();
-        })
-        .then(function (data) {
-            console.log("OpenWeather Current Forecast \n-----------")
-            console.log(data);
-            for (var i = 0; i < data.length; i++) {
-            }
-        })
+    city_name = chosen.val();
+    requestURL = "https://api.openweathermap.org/data/2.5/weather?q="+city_name+"&units=imperial&appid="+ApiKey;
+    fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log("OpenWeather Current Forecast \n-----------")
+        console.log(data);
+
+        wxImg.src = "https://api.openweather.org/img/wn/"+data.weather[0].icon+"@2px.png";
+        nowcast.append(wxImg);
+        
+        City.textContent = "City: " + data.name + " " + moment(Date(data.dt)).format("MMM DD, YYYY");
+        TempC.textContent = "Temp: " + data.main.temp + " F";
+        WindC.textContent = "Wind: " + data.wind.speed + " mph";
+        HumidC.textContent = "Humidity: " + data.main.humidity + "%";
+        CondC.textContent = "Conditions: " + data.weather[0].description;
+    })
 }
 
-fecthBtn.addEventListener('click', getApi);
+
+fetchBtn.addEventListener('click', getApi);
